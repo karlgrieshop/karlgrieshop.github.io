5b6d73c7bdf639c33a9d443a23c7210528cfe392
karlgrieshop <65978149+karlgrieshop@users.noreply.github.com>
2025-07-10 20:10:34 +0000

    Deploying to gh-pages from @ karlgrieshop/karlgrieshop.github.io@aa3746d3c9c10afd5e804f8a2367d54a915aacc1 🚀

--- _scripts/anchors.js
+++ _scripts/anchors.js
@@ -3,26 +3,26 @@
 */
 
 {
-  const onLoad = () => {
-    // for each heading
-    const headings = document.querySelectorAll(
-      "h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]"
-    );
-    for (const heading of headings) {
-      // create anchor link
-      const link = document.createElement("a");
-      link.classList.add("icon", "fa-solid", "fa-link", "anchor");
-      link.href = "#" + heading.id;
-      link.setAttribute("aria-label", "link to this section");
-      heading.append(link);
+  // const onLoad = () => {
+  //   // for each heading
+  //   const headings = document.querySelectorAll(
+  //     "h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]"
+  //   );
+  //   for (const heading of headings) {
+  //     // create anchor link
+  //     const link = document.createElement("a");
+  //     link.classList.add("icon", "fa-solid", "fa-link", "anchor");
+  //     link.href = "#" + heading.id;
+  //     link.setAttribute("aria-label", "link to this section");
+  //     heading.append(link);
 
-      // if first heading in the section, move id to parent section
-      if (heading.matches("section > :first-child")) {
-        heading.parentElement.id = heading.id;
-        heading.removeAttribute("id");
-      }
-    }
-  };
+  //     // if first heading in the section, move id to parent section
+  //     if (heading.matches("section > :first-child")) {
+  //       heading.parentElement.id = heading.id;
+  //       heading.removeAttribute("id");
+  //     }
+  //   }
+  // };
 
   // scroll to target of url hash
   const scrollToTarget = () => {
@@ -38,10 +38,13 @@
   };
 
   // after page loads
-  window.addEventListener("load", onLoad);
+  // window.addEventListener("load", onLoad);
   window.addEventListener("load", scrollToTarget);
   window.addEventListener("tagsfetched", scrollToTarget);
 
   // when hash nav happens
   window.addEventListener("hashchange", scrollToTarget);
 }
+  window.addEventListener("hashchange", scrollToTarget);
+}
+*/
